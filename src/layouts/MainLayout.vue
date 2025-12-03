6e6071ed70ef5217d7201c385406c238ef7871b6
genuinoLuiz <luizgustavo@pitom.com.br>
2025-12-03 13:21:40 -0400

    feat: add navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,13 +8,27 @@ const { smAndUp } = useDisplay()
 function onClick() {
     theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
+
+function avaliacao() {
+    window.open('https://app.pitom.com.br/avaliacoes', '_parent');
+}
+
+function repasse() {
+    window.open('https://app.pitom.com.br/repasse', '_parent');
+}
+
 </script>
 
 <template>
     <v-responsive class="min-h-screen">
         <v-app :theme="theme">
             <div :class="`d-flex flex-row justify-space-between align-center pa-4 ${smAndUp ? 'bg-primary' : ''}`">
-                <p :class="`text-h6 font-weight-bold  ${!smAndUp ? 'text-primary' : ''}`">P84</p>
+                <div class="d-flex align-center" >
+                <p :class="`text-h6 font-weight-bold mr-2  ${!smAndUp ? 'text-primary' : ''}`">P84</p>
+
+                    <v-btn variant="text" size="small" class="mx-2" @click="avaliacao">Avaliações</v-btn>
+                    <v-btn variant="text" size="small" @click="repasse">Repasse</v-btn>
+                </div>
 
                 <v-btn color="bg-component" :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
                     @click="onClick"></v-btn>
